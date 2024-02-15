b215a77d5ab1610bfa43842a95e2ceb2ac9d8a4b
홍길동 <hw05045@naver.com>
2024-02-15 20:18:35 +0900

    2023-07-20

---
+++ js/T1.js
@@ -0,0 +1,14 @@
+let swiper = new Swiper(".swiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
+// let swiper2 = new Swiper(".swiper2", {
+//   loop: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   }
+// });
